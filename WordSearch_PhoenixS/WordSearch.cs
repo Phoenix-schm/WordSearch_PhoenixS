3f5f5e0fcff1d4e3c0bee790a188918ff5f541b6
Phoenix-schm <ellaschmitt2016@gmail.com>
2025-02-14 16:18:09 -0600

    Created WordSearchDefault() that outputs a new row variable when called Player can now quit Testing WordSearchDefault through Main in the if statement Created function that changes Console.WriteLine() into WriteLine()

--- WordSearch_PhoenixS/WordSearch.cs
+++ WordSearch_PhoenixS/WordSearch.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Data;
 using System.IO;
 
 namespace WordSearch_PhoenixS
@@ -12,7 +14,7 @@ namespace WordSearch_PhoenixS
             string[] wordList = File.ReadAllLines(filePath);
 
             bool isPlaying = true;
-
+            string? stringInput;
             // each word search category
             string[] dogNicknames = WordsList.ReturnWordArray("DogNames", wordList);
             string[] colorsList = WordsList.ReturnWordArray("Colors", wordList);
@@ -31,31 +33,107 @@ namespace WordSearch_PhoenixS
                 dogNicknames, colorsList, poisonFlowers, thingsInRoom, thingsToEat, 
                 fabricTypes, mangaNames, fonts, dndMonsters, periodicElements
             };
+            string[] categoryNames =
+            {
+                "Dog Nicknames", "Colors", "Poisonous Flowers", "Things In My Room", "Things To Eat",
+                "Fabric Types", "Managa Names", "Fonts", "DND Monsters", "Periodic Table Elements"
+            };
 
 
-            Write("Welcome to the Amazing Word Search");
-            Write("You have ten categories to choose from");
+            WriteLine("Welcome to the Amazing Word Search");
+            WriteLine("You have ten categories to choose from:");
+            DisplayCategories(categoryNames);
+            WriteLine("--------------------------");
+            
 
             while(isPlaying)
             {
+                stringInput = InputCheck(categoryNames);
+                string input = stringInput.ToLower();
+                
+                if (input == categoryNames[0].ToLower() || input == 1.ToString())
+                {
+                    
+                    WordSearchDefault("01", out string row1);
+                }
+                else if (input == "quit" || input == 11.ToString())
+                {
+                    isPlaying = false;
+                }
 
             }
 
 
         }
 
-        static void WordSearchDefault()
+        static void WordSearchDefault(string rowNum, out string row)
         {
-            int num = 00;
+            string filler = ".";
+            string rowOutput = "";
+            string[] row1 = new string[20];
+            Array.Fill(row1, " " + filler + " ");
 
-            Write("Word Search Puzzle: ");
-            Write("  01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20");
+            Array[] wordSearch = { row1 };
+            WriteLine("Word Search Puzzle: ");
+            WriteLine("  01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20");
+
+            foreach(string word in row1)
+            {
+                rowOutput += word;
+            }
+            row = rowNum + rowOutput;
+            Console.Write(row);
+            //Write(num++ + row1.ToString());
            
         }
-        static void Write(string sentence)
+        static void WriteLine(string sentence)
         {
             Console.WriteLine(sentence);
         }
+        static void DisplayCategories(string[] categories)
+        {
+            int num = 1;
+            foreach (string category in categories)
+            {
+                WriteLine(num + ") " + category);
+                num++;
+            }
+            WriteLine(num + ") Quit");
+        }
+        static string InputCheck(string[] categories)
+        {
+            bool invalidInput = true;
+            while (invalidInput)
+            {
+                string? input = Console.ReadLine();
 
+                if (input != null)
+                {
+                    foreach (string category in categories)
+                    {
+                        if (input.ToLower() == category.ToLower())
+                        {
+                            invalidInput = false;
+                            return input;
+                        }
+                    }
+                    for (int i = 0; i <= categories.Count() + 1; i++)
+                    {
+                        if (input == i.ToString())
+                        {
+                            invalidInput = true;
+                            return input;
+                        }
+                    }
+                }
+                else
+                {
+                    invalidInput = true;
+                    WriteLine("Invalid input.");
+                }
+            }
+            return "Invalid input. Try again";
+
+        }
     }
 }

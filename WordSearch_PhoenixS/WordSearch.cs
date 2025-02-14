c43a95541dd0c60b560d660efef5cf146395da55
Phoenix-schm <ellaschmitt2016@gmail.com>
2025-02-14 12:48:53 -0600

    Add project files.

---
+++ WordSearch_PhoenixS/WordSearch.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.IO;
+
+namespace WordSearch_PhoenixS
+{
+    internal class WordSearch
+    {
+    }
+}
---
+++ WordSearch_PhoenixS/WordsList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace WordSearch_PhoenixS
+{
+    internal class WordsList
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader wordsFile = new StreamReader("words.txt");
+            string[] wordsList = File.ReadAllLines("words.txt");
+            //words.Close();
+        }
+    }
+}

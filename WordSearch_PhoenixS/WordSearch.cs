a6888c4b8736e895f93ce74ee24aa7f27f57a811
Phoenix-schm <ellaschmitt2016@gmail.com>
2025-02-14 15:16:39 -0600

    Added a way to create each word search category into its own array Created an array for each category Created an array of arrays holding all categorys Began creating the actual word search and game functionality with WordSearchDefault() and isPlaying boolean

--- WordSearch_PhoenixS/WordSearch.cs
+++ WordSearch_PhoenixS/WordSearch.cs
@@ -5,5 +5,57 @@ namespace WordSearch_PhoenixS
 {
     internal class WordSearch
     {
+        static void Main(string[] args)
+        {
+            // variable declarations
+            string filePath = "words.txt";
+            string[] wordList = File.ReadAllLines(filePath);
+
+            bool isPlaying = true;
+
+            // each word search category
+            string[] dogNicknames = WordsList.ReturnWordArray("DogNames", wordList);
+            string[] colorsList = WordsList.ReturnWordArray("Colors", wordList);
+            string[] poisonFlowers = WordsList.ReturnWordArray("PoisonousPlants", wordList);
+            string[] thingsInRoom = WordsList.ReturnWordArray("ThingsInMyRoom", wordList);
+            string[] thingsToEat = WordsList.ReturnWordArray("ThingsToEat", wordList);
+            string[] fabricTypes = WordsList.ReturnWordArray("FabricTypes", wordList);
+            string[] mangaNames = WordsList.ReturnWordArray("MangaList", wordList);
+            string[] fonts = WordsList.ReturnWordArray("Fonts", wordList);
+            string[] dndMonsters = WordsList.ReturnWordArray("DNDmonsters", wordList);
+            string[] periodicElements = WordsList.ReturnWordArray("PeriodicElements", wordList);
+
+            // array of the arrays
+            Array[] wordSearchArrays =
+            {
+                dogNicknames, colorsList, poisonFlowers, thingsInRoom, thingsToEat, 
+                fabricTypes, mangaNames, fonts, dndMonsters, periodicElements
+            };
+
+
+            Write("Welcome to the Amazing Word Search");
+            Write("You have ten categories to choose from");
+
+            while(isPlaying)
+            {
+
+            }
+
+
+        }
+
+        static void WordSearchDefault()
+        {
+            int num = 00;
+
+            Write("Word Search Puzzle: ");
+            Write("  01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20");
+           
+        }
+        static void Write(string sentence)
+        {
+            Console.WriteLine(sentence);
+        }
+
     }
 }
--- WordSearch_PhoenixS/WordsList.cs
+++ WordSearch_PhoenixS/WordsList.cs
@@ -5,11 +5,35 @@ namespace WordSearch_PhoenixS
 {
     internal class WordsList
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    //StreamReader wordsFile = new StreamReader("words.txt");
+        //    //string[] wordsList = File.ReadAllLines("words.txt");
+        //    //wordsFile.Close();
+        //}
+
+        /// <summary>
+        ///  Creates an array holding just the list of items for each word search category
+        /// </summary>
+        /// <param name="request"> the name of the category </param>
+        /// <param name="list"> the array it's accessing (should be an array made from words.txt file)</param>
+        /// <returns></returns>
+        public static string[] ReturnWordArray(string request, string[] list)
         {
-            using StreamReader wordsFile = new StreamReader("words.txt");
-            string[] wordsList = File.ReadAllLines("words.txt");
-            //words.Close();
+            string[] returnedList = new string[15];
+
+            if (list.Contains(request))
+            {
+                int position = Array.IndexOf(list, request);        // returns the index position of 'request' in list[]
+                int j = 0;
+                for (int i = position + 1; i <= position + 15; i++)
+                {
+                    returnedList[j] = list[i].ToString();           // fills in each element of 'returnedList' with the corresponding list[]
+                    j++;
+                }
+                return returnedList;
+            }
+            return returnedList;                            // will return a blank array of 15 lines
         }
     }
 }
